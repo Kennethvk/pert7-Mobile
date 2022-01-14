578a53758cba93172fea9a07476806fcb28d2f68
Kennethvk <kennethvk@yahoo.com>
2022-01-14 10:07:34 +0700

    fitur pertambahan

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,10 @@ public class Main {
 		// TODO Auto-generated constructor stub
 		System.out.println("Hello World");
 		System.out.println("Selamat datang di pertemuan 7 Mobile!"); //kita meng-echo ucapan selamat datang
+		
+		int sepuluh = 10;
+		sepuluh += 11;
+		System.out.println(sepuluh);
 	}
 
 	public static void main(String[] args) {
