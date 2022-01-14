678ae54426203a869faa3861d540364a4e8900f7
Kennethvk <kennethvk@yahoo.com>
2022-01-14 09:57:24 +0700

    melengkapkan file projek

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,12 @@ public class Main {
 
 	public Main() {
 		// TODO Auto-generated constructor stub
+		System.out.println("Hello World");
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		new Main();
 	}
 
 }
